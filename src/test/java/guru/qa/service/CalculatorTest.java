899c38daa7870c91739ce1b2a9fbd97a178893e3
Alexey <reni.gate@gmail.com>
2021-06-13 22:45:28 +0500

    added Minus, Mult, Div, Pow test for Calculator

--- src/test/java/guru/qa/service/CalculatorTest.java
+++ src/test/java/guru/qa/service/CalculatorTest.java
@@ -1,7 +1,7 @@
 package guru.qa.service;
 
 import guru.qa.service.impl.StringWriter;
-import guru.qa.service.mock.MockMultReader;
+import guru.qa.service.mock.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,9 +10,37 @@ class CalculatorTest {
     private Calculator calculator;
 
     @Test
-    void calculatorTest() {
+    void calculatorMultTest() {
         calculator = new Calculator(new MockMultReader(), new StringWriter()); // 3, 5, *
         String result = calculator.start();
         Assertions.assertEquals("3 * 5 = 15", result);
+    }
+
+    @Test
+    void calculatorSumTest() {
+        calculator = new Calculator(new MockSumReader(), new StringWriter()); // 3, 5, +
+        String result = calculator.start();
+        Assertions.assertEquals("3 + 5 = 8", result);
+    }
+
+    @Test
+    void calculatorDivTest() {
+        calculator = new Calculator(new MockDivReader(), new StringWriter()); // 10, 2, /
+        String result = calculator.start();
+        Assertions.assertEquals("10 / 2 = 5", result);
+    }
+
+    @Test
+    void calculatorMinusTest() {
+        calculator = new Calculator(new MockMinusReader(), new StringWriter()); // 10, 2, -
+        String result = calculator.start();
+        Assertions.assertEquals("10 - 2 = 8", result);
+    }
+
+    @Test
+    void calculatorPowTest() {
+        calculator = new Calculator(new MockPowReader(), new StringWriter()); // 2, 3, ^
+        String result = calculator.start();
+        Assertions.assertEquals("2 ^ 3 = 8", result);
     }
 }
